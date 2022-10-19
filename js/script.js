2f499411a6705cd330c9f5b647b6dac5f2d648eb
germancai <tw_german@hotmail.com>
2022-10-18 23:56:20 -0300

    js2

--- js/script.js
+++ js/script.js
@@ -1,40 +1,11 @@
-function compra(articulo,cantidad){
-    switch(articulo){
-        case "001":
-            return 3000*cantidad;
-            break;
-        case "002":
-            return 3000*cantidad;
-            break;
-        case "003":
-            return 4000*cantidad;
-            break;
-        case "004":
-            return 1000*cantidad;
-            break;
-        case "005":
-            return 3500*cantidad;
-            break;
-        case "006":
-            return 5000*cantidad;
-            break;
-        case "007":
-            return 1000*cantidad;
-            break;
-         case "008":
-             return 2000*cantidad;
-             break;
-         case "009":
-             return 1500*cantidad;
-             break;
-        default:
-            return 0;
-            break;
-                                                                        
-    }
-}
+
 
 let registro = false;
+let articulo = 0;
+let cantidad = 0;
+let total = 0;
+let acumulado= 0;
+let seguir = "si";
 
 do {
 const nombre = prompt("ingrese su nombre");
@@ -51,19 +22,54 @@ if ((nombre !=="")&& (apellido !="") && (usuario !="") && (clave !="")){
 
 }while(registro===false);
 
-let articulo = 0;
-let cantidad = 0;
-let total = 0;
-let acumulado= 0;
-let seguir = "si";
+class Productos{
+    constructor(nombre, precio, stock){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+    agregarStock(nuevoStock){
+        this.stock= this.stock + nuevoStock;
+    }
+    vender(venta){
+        if(venta<=this.stock){
+            this.stock = this.stock - venta;
+            total= this.precio * venta;
+            
+        }else{
+            console.log("la cantidad solicitada sobrepasa el stock actual, indique una cantidad menor");
+        }
+        return total;
+    }
+}
 
-//do{
-alert("Estos son los articulos disponibles: 001=camiseta Argentina titular 002=camiseta Argentina suplente 003=camiseta PSG 004=remera Paris blanca 005=camiseta suplente PSG 006= buzo con capucha negro 007=vaso aluminio 008=funko 009=taza");
-articulo = prompt("ingrese el numero del articulo");
-cantidad = prompt("ingrese las unidades que solicita");
-total= compra(articulo,cantidad);
-acumulado= acumulado+total;
-alert("el total de su compra es: $"+ acumulado);
-seguir = prompt("desea seguir agregando productos al carrito? si/no");
-//}while(seguir===si);
-//no me funciona el do while.
+const listaProductos = [];
+
+listaProductos.push(new Productos("camiseta titular Argentina 2021", 3000, 35));
+listaProductos.push(new Productos("camiseta suplente Argentina 2021", 3000, 25));
+listaProductos.push(new Productos("camiseta titular Psg", 4000, 20));
+listaProductos.push(new Productos("remera blanca Paris", 1000, 10));
+listaProductos.push(new Productos("camiseta suplente Psg", 3500, 15));
+listaProductos.push(new Productos("buzo con capucha negro", 5000, 4));
+listaProductos.push(new Productos("vaso aluminio 1lt", 1000, 20));
+listaProductos.push(new Productos("taza Messi", 1500, 30));
+
+
+console.log(listaProductos);
+listaProductos[0].agregarStock(15);
+console.log(listaProductos[0]);
+
+
+do{
+    articulo = prompt("ingrese el numero del articulo");
+    if(articulo< listaProductos.length){
+        cantidad = prompt("ingrese las unidades que solicita");
+        listaProductos[articulo].vender(cantidad);
+    } else{
+        console.log("el articulo seleccionado no esta en la lista");
+    }  
+    acumulado= acumulado+total;
+    alert("el total de su compra es: $"+ acumulado);
+    seguir = prompt("desea seguir agregando productos al carrito? si/no");
+}while(seguir==="si");
+
